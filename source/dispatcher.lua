9b34b7b152c42835cc2807ecc302d6d284a11059
Alessandro Delgado <adelgado1313@gmail.com>
2013-11-29 22:51:47 +0100

    add support for serving static files

--- source/dispatcher.lua
+++ source/dispatcher.lua
@@ -1,58 +1,62 @@
 local fs = require'luanode.fs'
 
-controllers = {}
+routes = {}
 
-controllers['/'] = function (request, response)
+routes['/'] = function (req, res)
 	fs.readFile('views/index.html', function(error, data)
 		if not error then
-			response:writeHead(200, {["Content-Type"] = "text/html"})
-			response:finish(data)
+			res:writeHead(200, {["Content-Type"] = "text/html"})
+			res:finish(data)
 		end
 	end)
 end
 
-controllers['/room'] = function (request, response)
-	response:writeHead(200, {["Content-Type"] = "text/plain"})
-	response:finish('/room')
+routes['/room'] = function (req, res)
+	res:writeHead(200, {["Content-Type"] = "text/plain"})
+	res:finish('/room')
 end
 
-controllers[404] = function (request, response)
+routes[404] = function (req, res)
 	fs.readFile('views/404.html', function(error, data)
 		if error then
-			controllers[500](request, response, data)
+			routes[500](req, res, data)
 		else
-			response:writeHead(404, {["Content-Type"] = "text/html"})
-			response:finish(data)
+			res:writeHead(404, {["Content-Type"] = "text/html"})
+			res:finish(data)
 		end
 	end)
 end
 
-controllers[500] = function (request, response, error)
-	response:writeHead(500, {["Content-Type"] = "text/html"})
-	response:finish(data)
+routes[500] = function (req, res, data)
+	res:writeHead(500, {["Content-Type"] = "text/html"})
+	res:finish(data)
 end
 
-return function (self, request, response)
-	if type(controllers[request.url]) == 'function' then
-		controllers[request.url](request, response)
-		print('Dispatching to controller for route ', request.url)
+routes['static'] = function (req, res)
+	filename = req.url:sub(2)
+
+	if filename:match('[%a%d/\._\-]+') then
+		fs.readFile(filename, function (error, data)
+			if error then
+				return routes[404](req, res, data)
+			else
+				print('Serving static file', filename)
+				res:writeHead(200, {["Content-Type"] = "text/plain"})
+				return res:finish(data)
+			end
+		end)
 	else
-		controllers[404](request, response)
+		routes[404](req, res, data)
 	end
+end
 
-	-- print'self'
-	-- for k, v in pairs(self) do
-	-- 	print(k, v)
-	-- end
-
-	-- print'request'
-	-- for k, v in pairs(request) do
-	-- 	print(k, v)
-	-- end
-
-	-- print'response'
-	-- for k, v in pairs(response) do
-	-- 	print(k, v)
-	-- end
-
+return function (self, req, res)
+	if type(routes[req.url]) == 'function' then
+		routes[req.url](req, res)
+		print('Dispatching to controller for route ', req.url)
+	elseif req.url:match('^/public') then
+		routes['static'](req, res)
+	else
+		routes[404](req, res)
+	end
 end
